96b1347ae28e14b3a4410a85556866a64127da9b
AllanBUSI <busi.travail@gmail.com>
2026-06-16 09:36:56 +0200

    aaaaaaaaaa

--- api/controllers/delete.controllers.js
+++ api/controllers/delete.controllers.js
@@ -1,4 +1,16 @@
+const { PrismaClient } = require("@prisma/client")
+module.exports = async (req, res) => {
 
-module.exports = (req, res) => {
-  res.send('/avis/:id')
+  try {
+      const prisma = new PrismaClient()
+      const deletedReview = await prisma.review.delete({
+        where: {
+          id: parseInt(req.params.id) 
+        }
+      })
+      return res.json({ message: "Avis supprimé avec succès", review: deletedReview })
+  } catch (error) {
+    console.error("Erreur DELETE /avis/:id :", error)
+    return res.status(500).json({ error: "Erreur serveur" })
+  }
 }
